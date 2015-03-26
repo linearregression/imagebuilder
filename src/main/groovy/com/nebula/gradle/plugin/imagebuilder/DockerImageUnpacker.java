e45c6b23880cf1def434dcb29ebb0d34ec7e6868
Shevek <shevek@nebula.com>
2015-03-26 10:26:45 -0700

    Consistent variable naming; use more Guava.

--- src/main/groovy/com/nebula/gradle/plugin/imagebuilder/DockerImageUnpacker.java
+++ src/main/groovy/com/nebula/gradle/plugin/imagebuilder/DockerImageUnpacker.java
@@ -24,7 +24,6 @@ import java.util.Map;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
 import org.apache.tools.tar.TarEntry;
 import org.apache.tools.tar.TarInputStream;
 import org.jgrapht.DirectedGraph;
@@ -42,6 +41,7 @@ import org.slf4j.LoggerFactory;
 public class DockerImageUnpacker {
 
     private static final Logger LOG = LoggerFactory.getLogger(DockerImageUnpacker.class);
+    private final ObjectMapper mapper = new ObjectMapper();
 
     public static enum DockerStorageBackend {
 
@@ -83,9 +83,8 @@ public class DockerImageUnpacker {
             @Nonnull File dstFile) throws IOException {
 
         //TODO: merge together the repositories
-        ObjectMapper mapper = new ObjectMapper();
-        LOG.debug("Existing repositories: " + existingRepositories.toString());
-        LOG.debug("New repositories: " + repositoriesJson.toString());
+        LOG.debug("Existing repositories: " + existingRepositories);
+        LOG.debug("New repositories: " + repositoriesJson);
 
         //Overwrite that shit
         existingRepositories.putAll(repositoriesJson);
@@ -95,48 +94,36 @@ public class DockerImageUnpacker {
         mapper.writeValue(dstFile, existingRepositories);
     }
 
-    private void createParentDirs(@Nonnull File f) {
-        Path p = f.toPath().getParent();
-        if (!p.toFile().exists())
-            p.toFile().mkdirs();
-    }
-
-    private void unpackEntry(@Nonnull TarEntry t,
-            @Nonnull File baseDirectory,
-            @Nonnull TarInputStream inputStream) throws FileNotFoundException, IOException {
-        LOG.debug("unpacking: " + t.getName());
-        File destFile = new File(baseDirectory, t.getName());
-        if (t.isDirectory()) {
-            if (!destFile.exists()) {
-                LOG.debug("Creating directory : " + destFile.getAbsolutePath());
-                destFile.mkdirs();
+    private void unpackLayerEntry(
+            @Nonnull TarInputStream imageTarStream,
+            @Nonnull TarEntry imageTarEntry,
+            @Nonnull File baseDirectory
+    ) throws FileNotFoundException, IOException {
+        File imageTarEntryFile = new File(baseDirectory, imageTarEntry.getName());
+        LOG.debug("Unpacking " + imageTarEntry.getName() + " into " + imageTarEntryFile.getAbsolutePath());
+        if (imageTarEntry.isDirectory()) {
+            if (!imageTarEntryFile.isDirectory()) {
+                FileUtils.forceMkdir(imageTarEntryFile);
             }
         } else {
             //Lets try to be somewhat efficient and write 128Kb writes
-            createParentDirs(destFile);
+            Files.createParentDirs(imageTarEntryFile);
+            Files.asByteSink(imageTarEntryFile).writeFrom(imageTarStream);
 
-            try (FileOutputStream fout = new FileOutputStream(destFile)) {
-                IOUtils.copy(inputStream, fout);
-            }
-
-            try (TarInputStream layerInputStream = new TarInputStream(new FileInputStream(destFile))) {
-                //Unpack this in the current directory
-
-                TarEntry entry;
-                while ((entry = layerInputStream.getNextEntry()) != null) {
-                    if ("./".equals(entry.getName()))
+            try (TarInputStream layerTarStream = new TarInputStream(new FileInputStream(imageTarEntryFile))) {
+                TarEntry layerTarEntry;
+                while ((layerTarEntry = layerTarStream.getNextEntry()) != null) {
+                    if ("./".equals(layerTarEntry.getName()))
                         continue;
-                    File out = new File(baseDirectory, entry.getName());
 
-                    if (entry.isDirectory()) {
-                        out.mkdirs();
-                        continue;
-                    }
+                    File layerTarEntryFile = new File(baseDirectory, layerTarEntry.getName());
 
-                    //Ensure the parent directories exist for this file
-                    createParentDirs(out);
-                    try (FileOutputStream fout = new FileOutputStream(out)) {
-                        IOUtils.copy(layerInputStream, fout);
+                    if (layerTarEntry.isDirectory()) {
+                        FileUtils.forceMkdir(layerTarEntryFile);
+                    } else {
+                        //Ensure the parent directories exist for this file
+                        Files.createParentDirs(layerTarEntryFile);
+                        Files.asByteSink(layerTarEntryFile).writeFrom(layerTarStream);
                     }
                 }
             }
@@ -151,15 +138,14 @@ public class DockerImageUnpacker {
             IOException,
             DirectedAcyclicGraph.CycleFoundException {
 
-        ObjectMapper mapper = new ObjectMapper();
         DirectedAcyclicGraph<String, DefaultEdge> root = new DirectedAcyclicGraph<>(DefaultEdge.class);
 
         Map<String, Object> tarRepositories = new HashMap<>();
 
-        String treeRoot = "";
+        String treeRoot = null;
 
         String existingRepoInfo = Files.toString(
-                new File("/tmp/repositories-aufs"),
+                new File(dstDir, "repositories-aufs"),
                 Charset.defaultCharset());
 
         Map<String, Object> existingRepositories = mapper.readValue(
@@ -168,21 +154,21 @@ public class DockerImageUnpacker {
                 });
 
         try (FileInputStream in = FileUtils.openInputStream(srcFile)) {
-            TarInputStream tarInputStream = new TarInputStream(new BufferedInputStream(in));
-            TarEntry tarEntry;
-            while ((tarEntry = tarInputStream.getNextEntry()) != null) {
+            TarInputStream imageTarStream = new TarInputStream(new BufferedInputStream(in));
+            TarEntry imageTarEntry;
+            while ((imageTarEntry = imageTarStream.getNextEntry()) != null) {
                 /*
                  117ee323aaa9d1b136ea55e4421f4ce413dfc6c0cc6b2186dea6c88d93e1ad7c/VERSION
                  117ee323aaa9d1b136ea55e4421f4ce413dfc6c0cc6b2186dea6c88d93e1ad7c/json
                  117ee323aaa9d1b136ea55e4421f4ce413dfc6c0cc6b2186dea6c88d93e1ad7c/layer.tar
                  */
-                String name = tarEntry.getName();
+                String name = imageTarEntry.getName();
 
                 if (name.endsWith("/layer.tar")) {
-                    unpackEntry(tarEntry, new File(dstDir, "diff"), tarInputStream);
+                    unpackLayerEntry(imageTarStream, imageTarEntry, new File(dstDir, "diff"));
                 } else if (name.endsWith("/json")) {
                     //This is just so we can read the file and then throw it away.
-                    byte[] data = ByteStreams.toByteArray(tarInputStream);
+                    byte[] data = ByteStreams.toByteArray(imageTarStream);
                     DockerTarballMetadata value = mapper.readValue(data, DockerTarballMetadata.class);
                     if (value.parent == null) {
                         //This is the root
@@ -199,16 +185,16 @@ public class DockerImageUnpacker {
                     }
                 } else if ("/repositories".equals(name)) {
                     //Save this file for later so we can add all the images to it
-                    byte[] data = ByteStreams.toByteArray(tarInputStream);
+                    byte[] data = ByteStreams.toByteArray(imageTarStream);
                     tarRepositories = mapper.readValue(data, new TypeReference<Map<String, Object>>() {
                     });
                     LOG.debug("Repositories: " + tarRepositories.toString());
                 }
             }
         }
-        if (treeRoot.isEmpty()) {
+
+        if (treeRoot == null)
             throw new IllegalStateException("Root of docker container tree not found!");
-        }
 
         //Save the RepositoriesJson file back out
         createLayersFiles(root, new File(dstDir, "layers"), treeRoot);
@@ -242,6 +228,7 @@ public class DockerImageUnpacker {
     public static void main(String[] args) throws LibGuestFSException,
             IOException, FileNotFoundException, DirectedAcyclicGraph.CycleFoundException {
         DockerImageUnpacker cephOperations = new DockerImageUnpacker();
-        cephOperations.unpackAufsDockerImage(new File("/home/chris/docker-image.tar"), new File("/tmp"));
+        File tmpDir = Files.createTempDir();
+        cephOperations.unpackAufsDockerImage(new File("/home/chris/docker-image.tar"), tmpDir);
     }
 }
